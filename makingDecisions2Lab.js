c11af24eb53ec2fb1e0dd5a4e88c9fc5380a1d8a
tdelpidio <tdelpidio@gmail.com>
2022-05-26 19:01:15 -0400

    Problem 11 solved.

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -217,3 +217,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for(let i = 1; i < 100; i++){
+  if(i % 5 === 0 && i % 3 === 0){
+    console.log("DevMountain");
+  } else if (i % 5 === 0){
+    console.log("Mountain")
+  } else if (i % 3 === 0){
+    console.log("Dev")
+} else {
+  console.log(i)
+}
+}
